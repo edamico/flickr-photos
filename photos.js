11e1e5d000be0ec6f982ec75240098a1527c30b7
edamico <esteban@nan-labs.com>
2013-09-01 23:33:54 -0300

    Favorite toggle implemented

--- photos.js
+++ photos.js
@@ -55,18 +55,86 @@ var setupPhotos = (function ($) {
         });
     }
 
+    function getFavorites () {
+      var savedList = localStorage.getItem('favorites');
+      var favorites = [];
+      if (savedList){
+        favorites = JSON.parse(savedList);
+      }
+      return favorites;
+    }
+
+    function isFavorite(url){
+        var favorites = getFavorites();
+        return (favorites.indexOf(url) >= 0);
+    }
+
+    function addFavorite(url){
+        var favorites = getFavorites();
+        favorites.push(url);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+
+    function removeFavorite(url){
+        var favorites = getFavorites();
+        var index = favorites.indexOf(url);
+        favorites.splice(index, 1);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+
+
+    function getIconClass(url){
+        var className = 'icon-heart-empty icon-border';
+        if (isFavorite(url)){
+            className = 'icon-heart icon-border'
+        }
+        return className;
+    }
+
+    function toggleFavorite(icon){
+        var url = icon.getAttribute('data-url');
+        if (isFavorite(url)){
+            removeFavorite(url);
+        }else{
+            addFavorite(url);
+        };
+        icon.className =  getIconClass(url);
+
+    }
+
     function renderPhoto (photo) {
         var img = new Image();
         img.src = photo;
         return img;
     }
 
+    function onFavoriteToggle (event) {
+        var icon = event.currentTarget;
+        toggleFavorite(icon);
+    }
+
+    function createFavoriteIcon(img){
+        var icon = document.createElement('i');
+        icon.className = getIconClass(img.src);
+        icon.setAttribute("data-url", img.src);
+        icon.addEventListener('click', onFavoriteToggle);
+        return icon;
+    }
+
+    function createPhotoWrapper(img){
+        var elm = document.createElement('div');
+        elm.className = 'photo';        
+        var icon = createFavoriteIcon(img);
+        elm.appendChild(img);
+        elm.appendChild(icon);
+        return elm;
+    }
+
     function imageAppender (id) {
         var holder = document.getElementById(id);
+        var self = this;
         return function (img) {
-            var elm = document.createElement('div');
-            elm.className = 'photo';
-            elm.appendChild(img);
+            var elm = createPhotoWrapper(img);
             holder.appendChild(elm);
         };
     }
